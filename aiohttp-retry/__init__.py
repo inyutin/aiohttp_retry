6348c7e90c8eb59cd7f91f636f465b466f6c94b6
Dmitry Inuytin <Inyutin.DA@phystech.edu>
2019-12-27 15:33:18 +0300

    Update __init__.py

--- aiohttp-retry/__init__.py
+++ aiohttp-retry/__init__.py
@@ -1,97 +1,72 @@
-import logging
-from functools import partial
-
+import asyncio
 import aiohttp
 
 
-class RetryClient:
-    def __init__(self, client=None, retry_attempts=3, retry_start_timeout=0,
-                 retry_max_timeout=60, retry_factor=2, retry_for_statuses=None, retry_exceptions=None,
-                 logger=None):
+class _RequestContext:
+    def __init__(self, client: aiohttp.ClientSession, url: str, retry_attempts: int,
+                 retry_start_timeout: float = 0.1, retry_max_timeout: float = 30,
+                 retry_factor: float = 2, retry_for_statuses=None, retry_exceptions=None, **kwargs):
+        self._client = client
+        self._url = url
 
-        if client:
-            self._client = client
-        else:
-            self._client = aiohttp.ClientSession()
+        self._retry_attempts = retry_attempts  # How many times we should retry
+        self._retry_start_timeout = retry_start_timeout  # Base timeout time, than it exponentially grow
+        self._retry_max_timeout = retry_max_timeout  # Max possible timeout between tries
+        self._retry_factor = retry_factor  # How much we increase timeout each time
 
-        self._retry_attempts = retry_attempts
-        self._retry_max_timeout = retry_max_timeout
-        self._retry_start_timeout = retry_start_timeout
-        self._retry_exceptions = retry_exceptions
-        self._retry_for_statuses = retry_for_statuses
-        self._retry_factor = retry_factor
-        self.logger = logger
+        self._retry_for_statuses = retry_for_statuses  # On which statuses we should retry
+        self._retry_exceptions = retry_exceptions  # On which exceptions we should retry
 
-    def fetch(self, request, **kwargs):
-        kwargs.setdefault("retry_start_timeout", self._retry_start_timeout)
-        kwargs.setdefault("attempts", self._retry_attempts)
-        kwargs.setdefault("retry_exceptions", self._retry_exceptions)
-        kwargs.setdefault("retry_factor", self._retry_factor)
-        kwargs.setdefault("logger", self.logger)
-        kwargs.setdefault("retry_for_statuses", self._retry_for_statuses)
-        kwargs.setdefault("retry_max_timeout", self._retry_max_timeout)
-        return http_retry(self._client, request, **kwargs)
+        self._kwargs = kwargs
+
+        self._current_attempt = 0
+        self._response = None
+
+    def _exponential_timeout(self):
+        timeout = self._retry_start_timeout * (self._retry_factor ** (self._current_attempt - 1))
+        return min(timeout, self._retry_max_timeout)
+
+    def _check_code(self, code):
+        return 500 <= code <= 599 or code in self._retry_for_statuses
+
+    async def _do_request(self):
+        try:
+            self._current_attempt += 1
+            response = await self._client.get(self._url, **self._kwargs)
+            code = response.status
+            if self._current_attempt < self._retry_attempts and self._check_code(code):
+                retry_wait = self._exponential_timeout()
+                await asyncio.sleep(retry_wait)
+                await self._do_request()
+            self._response = response
+            return response
+
+        except Exception as e:
+            retry_wait = self._exponential_timeout()
+            if isinstance(e, self._retry_exceptions) and self._current_attempt < self._retry_attempts:
+                await asyncio.sleep(retry_wait)
+                await self._do_request()
+
+            raise e
+
+    async def __aenter__(self):
+        return await self._do_request()
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if self._response is not None:
+            self._response.close()
 
 
-def http_retry(client, request, raise_error=True, attempts=3, retry_start_timeout=0,
-               retry_max_timeout=60, retry_factor=2, retry_for_statuses=None, retry_exceptions=None,
-               logger=None, **kwargs):
-    attempt = 0
+class RetryClient(object):
+    def __init__(self, client: aiohttp.ClientSession = aiohttp.ClientSession()) -> None:
 
-    if not retry_exceptions:
-        retry_exceptions = ()
+        self._client = client
 
-    if not retry_for_statuses:
-        retry_for_statuses = ()
+    def get(self, request: str, retry_attempts: int = 3, retry_start_timeout: float = 0.1,
+            retry_max_timeout: float = 30,  retry_factor: float = 2, retry_for_statuses: set = (),
+            retry_exceptions: set = (), **kwargs) -> _RequestContext:
+        return _RequestContext(self._client, request, retry_attempts, retry_start_timeout, retry_max_timeout,
+                               retry_factor, retry_for_statuses, retry_exceptions, **kwargs)
 
-    if not logger:
-        logger = logging.getLogger("RetryClient")
-
-    def _do_request(attempt):
-        http_future = client.get(request, raise_error=False, **kwargs)
-        http_future.add_done_callback(partial(handle_future, attempt))
-
-    def handle_future(attempt, future_response):
-        attempt += 1
-        exception = future_response.exception()
-        if exception:
-            return handle_exception(attempt, exception)
-
-        handle_response(attempt, future_response.result())
-
-    def check_code(code):
-        return code >= 500 and code <= 599 or code in retry_for_statuses
-
-    def exponential_timeout(attempt):
-        timeout = retry_start_timeout * (retry_factor ** (attempt - 1))
-        return min(timeout, retry_max_timeout)
-
-    def handle_response(attempt, result):
-        if result.error and attempt < attempts and check_code(result.code):
-            retry_wait = exponential_timeout(attempt)
-            logger.warning(
-                u"attempt: %d, %s request failed: %s, body: %s",
-                attempt,
-                result.effective_url,
-                result.error,
-                repr(result.body),
-            )
-            return ioloop.call_later(retry_wait, lambda: _do_request(attempt))
-
-        if raise_error and result.error:
-            return future.set_exception(result.error)
-
-        future.set_result(result)
-
-    def handle_exception(attempt, exception):
-        retry_wait = exponential_timeout(attempt)
-        logger.warning(
-            u"attempt: %d, request failed with exception: %s", attempt, exception
-        )
-        if isinstance(exception, retry_exceptions) and attempt < attempts:
-            return ioloop.call_later(retry_wait, lambda: _do_request(attempt))
-
-        return future.set_exception(exception)
-
-    _do_request(attempt)
-    return future
+    async def close(self):
+        await self._client.close()

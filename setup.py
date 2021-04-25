b41dd3022d3dbac3e97456d694bff888d4d87a91
Dmitry <inyutin@hicebank.ru>
2021-04-25 17:27:55 +0300

    bump version to v2.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='aiohttp_retry',
-    version='2.3.5',
+    version='2.4.0',
     description='Simple retry client for aiohttp',
     long_description=long_description,
     long_description_content_type="text/markdown",
